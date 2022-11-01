61cf2cf2b0e5bf51a5abb341b3bd8d78c9806e90
KimJaeGeun <jgk9612@gmail.com>
2022-11-02 01:47:37 +0900

    feat: Apply AutoIncrease in Database
    no 를 자동으로 증가시키게 하기위한 작업

--- src/main/java/com/board/dao/BoardDao.java
+++ src/main/java/com/board/dao/BoardDao.java
@@ -20,15 +20,14 @@ public class BoardDao {
                 env.get("DB_USER"),
                 env.get("DB_PASSWORD"));
 
-        PreparedStatement pstmt = c.prepareStatement("INSERT INTO `board`.posts(no, title, content, author) VALUES (?, ?, ?, ?)");
+        PreparedStatement pstmt = c.prepareStatement("INSERT INTO `board`.posts(title, content, author) VALUES (?, ?, ?)");
 
-        // TODO: No를 int로 받는 법 찾기, Mysql에서 AI 설정해서 자동으로 증가하게 하기
-        pstmt.setString(1, boardVo.getNo());
-        pstmt.setString(2, boardVo.getTitle());
-        pstmt.setString(3, boardVo.getContent());
-        pstmt.setString(4, boardVo.getAuthor());
+        pstmt.setString(1, boardVo.getTitle());
+        pstmt.setString(2, boardVo.getContent());
+        pstmt.setString(3, boardVo.getAuthor());
 
         pstmt.executeUpdate();
+
 
         pstmt.close();
         c.close();
--- src/test/java/com/board/dao/BoardDaoTest.java
+++ src/test/java/com/board/dao/BoardDaoTest.java
@@ -18,15 +18,16 @@ class BoardDaoTest {
     @BeforeEach
     void setUp() {
 
-        this.post1 = new BoardVo("0", "글 제목 1", "글 내용 1234", "김재근");
-        this.post2 = new BoardVo("1", "글 제목 2", "글 내용 3456", "김재근");
-        this.post3 = new BoardVo("2", "글 제목 2", "글 내용 6789", "가가가");
+        this.post1 = new BoardVo("글 제목 1", "글 내용 1234", "김재근");
+        this.post2 = new BoardVo( "글 제목 2", "글 내용 3456", "김재근");
+        this.post3 = new BoardVo( "글 제목 2", "글 내용 6789", "가가가");
         System.out.println("BeforeEach");
     }
 
     @Test
     @DisplayName("Insert가 잘 되는지 테스트하는 메소드")
     void insertTest() throws SQLException {
+
         BoardDao boardDao = new BoardDao();
 
         boardDao.insert(post1);

fadeefc35a84e1da531fe7fdb55b3e8dd85a0226
KimJaeGeun <jgk9612@gmail.com>
2022-11-02 03:24:32 +0900

    test: Add Select Test Code but occurred error
    id 로 select하는 로직인데, id가 없는 문제
    (id 없는 id 파티...?)

--- src/test/java/com/board/dao/BoardDaoTest.java
+++ src/test/java/com/board/dao/BoardDaoTest.java
@@ -18,9 +18,9 @@ class BoardDaoTest {
     @BeforeEach
     void setUp() {
 
-        this.post1 = new BoardVo("글 제목 1", "글 내용 1234", "김재근");
-        this.post2 = new BoardVo( "글 제목 2", "글 내용 3456", "김재근");
-        this.post3 = new BoardVo( "글 제목 2", "글 내용 6789", "가가가");
+        this.post1 = new BoardVo(1,"글 제목 1", "글 내용 1234", "김재근");
+        this.post2 = new BoardVo(2,"글 제목 2", "글 내용 3456", "김재근");
+        this.post3 = new BoardVo(3, "글 제목 2", "글 내용 6789", "가가가");
         System.out.println("BeforeEach");
     }
 
@@ -33,5 +33,11 @@ class BoardDaoTest {
         boardDao.insert(post1);
         boardDao.insert(post2);
         boardDao.insert(post3);
+
+        //Todo: id를 지정 안했을 때 getter를 사용하지 못하는 문제
+        BoardVo post = boardDao.selectByNo(post1.getNo(post1));
+        assertEquals();
+
+
     }
 }
